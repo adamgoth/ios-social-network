ba6129aa08af1a8a6d90b68c0ca511ddfc2c2bbb
Adam Goth <adamgoth@gmail.com>
2016-07-22 17:36:13 -0600

    Adds custom table view cell components, adds table view boilerplate code

---
+++ ios-social-network/PostCell.swift
@@ -0,0 +1,33 @@
+//
+//  PostCell.swift
+//  ios-social-network
+//
+//  Created by Adam Goth on 7/22/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
