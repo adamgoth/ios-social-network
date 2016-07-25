6ff3a2a5b162bd414f880f35e04639af4b1e16f7
Adam Goth <adamgoth@gmail.com>
2016-07-25 06:56:20 -0600

    Adds model for user posts, parses user posts data from Firebase

--- ios-social-network/DataService.swift
+++ ios-social-network/DataService.swift
@@ -14,9 +14,14 @@ class DataService {
     static let ds = DataService()
     
     private var _ref = FIRDatabase.database().reference()
+    private var _posts_ref = FIRDatabase.database().referenceFromURL("https://ios-social-network.firebaseio.com/posts")
     
     var ref: FIRDatabaseReference {
         return _ref
+    }
+    
+    var posts_ref: FIRDatabaseReference {
+        return _posts_ref
     }
     
     func createFirebaseUser(uid: String, user: Dictionary<String, String>) {
--- ios-social-network/FeedVC.swift
+++ ios-social-network/FeedVC.swift
@@ -7,16 +7,36 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        DataService.ds.posts_ref.observeEventType(FIRDataEventType.Value, withBlock: { (snapshot) in
+            print(snapshot)
+            
+            self.posts = []
+            if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshots {
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, dictionary: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            
+            
+            self.tableView.reloadData()
+        })
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -24,10 +44,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print(post.postDescription)
+        
         return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
     }
 
